c434b71c238ad59a34391986da88346bac17d4eb
My Nguyen <s3905625@rmit.edu.vn>
2026-05-15 23:27:45 +1000

    update

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 let text = document.getElementById('text');
-let back = document.getElementById('back');
-let mid = document.getElementById('mid');
-let front = document.getElementById('front');
 
 //Initially, I wanted the text to disappear behind the grassy hill when scrolled down, but I encountered some problems with it scrolling endlessly.
 //So I decided to have the text move upwards when scrolling.
